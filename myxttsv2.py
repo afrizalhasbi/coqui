389f24989d8a122ec69845780368dea81e960a2f
Afrizal Hasbi Azizy <afrizal.azizy@gmail.com>
2024-12-11 20:31:45 +0700

    Update myxttsv2.py

--- myxttsv2.py
+++ myxttsv2.py
@@ -48,7 +48,7 @@ config_dataset = BaseDatasetConfig(
 # Logging parameters
 RUN_NAME = f"xtts_{random.randint(10_000, 99_999)}"
 PROJECT_NAME = "XTTS_trainer"
-DASHBOARD_LOGGER = args.logger if if args.logger is not None else "tensorboard"
+DASHBOARD_LOGGER = args.logger if args.logger is not None else "tensorboard"
 LOGGER_URI = None
 
 # Set here the path that the checkpoints will be saved.
